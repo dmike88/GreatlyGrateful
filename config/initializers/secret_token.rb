6ec4cab4807489800c08927071655b6726ad991f
Dan <danlasker@gmail.com>
2014-03-22 17:11:23 -0700

    Starting GreatlyGrateful

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greatlygrateful::Application.config.secret_key_base = '963f29d67560d0423e7f85074fb46543e1c19b06b3d165eda04cf0e7779827f89b15fd5f54a09839ab41ed2df15358457c760c599312506a669569b00eee3632'
